13e544f91928f2c91394903d1a8b24ebde2595b9
Simon Crouzet <simon.crouzet@laposte.net>
2020-02-07 16:45:01 +0100

    Fichiers SableCC, et initialisation de CompilerTest

---
+++ src/CompilerTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompilerTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    // TODO: Automatiser test pour tous les fichiers tests
+    @org.junit.jupiter.api.Test
+    void main() {
+        //Compiler.main("test/input/add1.l");
+    }
+}
