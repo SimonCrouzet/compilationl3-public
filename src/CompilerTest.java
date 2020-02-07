5e5e1b5390280d39f6ab32fcaf1d6ee3d23cf516
Simon Crouzet <simon.crouzet@laposte.net>
2020-02-07 17:03:47 +0100

    Implémentation d'une méthode de test général utilisant tous les fichiers fournis

--- src/CompilerTest.java
+++ src/CompilerTest.java
@@ -1,14 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompilerTest {
+    ArrayList<String> listOfFiles;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-
+        Stream<Path> walk = null;
+        try {
+            walk = Files.walk(Paths.get("C:\\Users\\Simon\\Documents\\L3 Informatique (Luminy - Växjö)" +
+                    "\\Semestre 6" +
+                    " - Luminy\\Compilation\\TP\\TP1\\compilationl3-public\\test\\input"));
+        } catch (IOException e) {
+            System.out.println("Please update tests path if you want to proceed to the automated tests");
+        }
+        listOfFiles = (ArrayList<String>) walk.filter(Files::isRegularFile)
+                .map(Path::toString).collect(Collectors.toList());
     }
 
-    // TODO: Automatiser test pour tous les fichiers tests
     @org.junit.jupiter.api.Test
-    void main() {
+    void TestLFiles() {
+        for (String file : listOfFiles) {
+            if (file.contains(".l")) {
+                String[] args = new String[1];
+                args[0] = file;
+                Compiler.main(args);
+            }
+        }
         //Compiler.main("test/input/add1.l");
     }
 }
