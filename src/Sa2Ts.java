6de8b23914751cfda94b2cc910a116805ecc6bc2
Simon Crouzet <simon.crouzet@laposte.net>
2020-02-14 20:33:59 +0100

    Séance TP 14/02: Avancement de Sa2Ts

--- src/Sa2Ts.java
+++ src/Sa2Ts.java
@@ -1,8 +1,12 @@
 import sa.*;
 import ts.Ts;
+import ts.TsItemVar;
 
 public class Sa2Ts extends SaDepthFirstVisitor {
+    enum Context {LOCAL, PARAM, GLOBAL}
     private Ts tableGlobale;
+    private Ts tableLocaleCourante = null;
+    private Context context;
 
     public Sa2Ts() {
         this.tableGlobale = new Ts();
@@ -10,23 +14,105 @@ public class Sa2Ts extends SaDepthFirstVisitor {
 
     @Override
     public Object visit(SaDecTab node) {
-        return super.visit(node);
+        // node.accept(this);
+
+        // TODO: Statuer sur le cas des tableaux de 1 case
+        if (node.getTaille()<2)
+            throw new IllegalCallerException("Array declared isn't enough large!");
+
+        if (context.equals(Context.LOCAL)) {
+            if (tableLocaleCourante.getVar(node.getNom()) != null)
+                throw new IllegalCallerException("Tab " + node.getNom() + " already exists!");
+            if (tableGlobale.getVar(node.getNom()) != null)
+                System.out.println("Warning: Local tab " + node.getNom() + " will cover global tab.");
+            tableLocaleCourante.addVar(node.getNom(), node.getTaille());
+        }
+        else if (context.equals(Context.PARAM)) {
+            /*if (tableGlobale.getVar(node.getNom()) != null)
+                System.out.println("Warning: Local param " + node.getNom() + " will cover global var.");*/
+            tableLocaleCourante.addParam(node.getNom());
+        }
+        else if (context.equals(Context.GLOBAL)) {
+            if (tableGlobale.getVar(node.getNom()) != null)
+                throw new IllegalCallerException("Global tab " + node.getNom() + " already exists!");
+            tableGlobale.addVar(node.getNom(), node.getTaille());
+        }
+        return null;
     }
 
     @Override
     public Object visit(SaDecFonc node) {
-        tableGlobale.ad
-        return super.visit(node);
+        defaultIn(node);
+        tableLocaleCourante = new Ts();
+
+        context = Context.PARAM;
+        if(node.getParametres() != null) node.getParametres().accept(this);
+
+        context = Context.LOCAL;
+        if(node.getVariable() != null) node.getVariable().accept(this);
+
+        node.getCorps().accept(this);
+
+        context = Context.GLOBAL;
+        this.tableGlobale.addFct(node.getNom(), tableLocaleCourante.nbArg(), tableLocaleCourante, node);
+        defaultOut(node);
+        return null;
     }
 
     @Override
     public Object visit(SaDecVar node) {
-        return super.visit(node);
+        // node.accept(this);
+        if (context.equals(Context.LOCAL)) {
+            if (tableLocaleCourante.getVar(node.getNom()) != null)
+                throw new IllegalCallerException("Var " + node.getNom() + " already exists!");
+            if (tableGlobale.getVar(node.getNom()) != null)
+                System.out.println("Warning: Local var " + node.getNom() + " will cover global var.");
+            tableLocaleCourante.addVar(node.getNom(), 1);
+        }
+        else if (context.equals(Context.PARAM)) {
+            /*if (tableGlobale.getVar(node.getNom()) != null)
+                System.out.println("Warning: Local param " + node.getNom() + " will cover global var.");*/
+            tableLocaleCourante.addParam(node.getNom());
+        }
+        else if (context.equals(Context.GLOBAL)) {
+            if (tableGlobale.getVar(node.getNom()) != null)
+                throw new IllegalCallerException("Global var " + node.getNom() + " already exists!");
+            tableGlobale.addVar(node.getNom(), 1);
+        }
+        return null;
     }
 
     @Override
     public Object visit(SaVarSimple node) {
-        return super.visit(node);
+        // node.accept(this);
+        if (context.equals(Context.LOCAL)) {
+            TsItemVar varSimple = tableLocaleCourante.getVar(node.getNom());
+
+            if (varSimple == null) {
+                varSimple = tableGlobale.getVar(node.getNom());
+
+                if (varSimple == null)
+                    throw new IllegalCallerException("Var " + node.getNom() + " doesn't exist!");
+            }
+            node.tsItem = varSimple;
+        }
+        else if (context.equals(Context.PARAM)) {
+            //TODO: A Vérifier
+            TsItemVar varSimple = tableLocaleCourante.getVar(node.getNom());
+
+            if (varSimple == null)
+                throw new IllegalCallerException("Param " + node.getNom() + " doesn't exist!");
+
+            node.tsItem = varSimple;
+        }
+        else if (context.equals(Context.GLOBAL)) {
+            TsItemVar varSimple = tableGlobale.getVar(node.getNom());
+            if (varSimple == null)
+                throw new IllegalCallerException("Global var " + node.getNom() + " doesn't exist!");
+
+            node.tsItem = varSimple;
+        }
+        return null;
     }
 
     @Override
