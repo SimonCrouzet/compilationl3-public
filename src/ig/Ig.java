f902fc966edcb036f1f4201875ed5517ebe1b961
Simon Crouzet <simon.crouzet@laposte.net>
2020-04-10 12:07:39 +0200

    Ig : travail du 10/04 matin

--- src/ig/Ig.java
+++ src/ig/Ig.java
@@ -16,20 +16,70 @@ public class Ig {
 
     
     public Ig(FgSolution fgs){
-	this.fgs = fgs;
- 	this.graph = new Graph();
-	this.nasm = fgs.nasm;
-	this.regNb = this.nasm.getTempCounter();
-	this.int2Node = new Node[regNb];
-	this.build();
+		this.fgs = fgs;
+		this.graph = new Graph();
+		this.nasm = fgs.nasm;
+		this.regNb = this.nasm.getTempCounter();
+		this.int2Node = new Node[regNb];
+		this.construction();
     }
 
     public void construction(){
+		initializeNodes();
+		for (NasmInst inst : nasm.listeInst) {
+			manageEdge(fgs.in.get(inst));
+			manageEdge(fgs.out.get(inst));
+		}
     }
+
+    private void initializeNodes() {
+		for (NasmInst inst : nasm.listeInst)
+			graph.newNode();
+		int2Node = graph.nodeArray();
+	}
+
+	// TODO: rename
+	private void manageEdge(IntSet refSet) {
+    	if (refSet.getSize() < 2)
+    		return;
+    	for (int i=0 ; i < nasm.getTempCounter()-1 ; i++) {
+    		if (!refSet.isMember(i))
+    			continue;
+    		for (int j= i+1 ; j < nasm.getTempCounter() ; j++) {
+				if (!refSet.isMember(j))
+					continue;
+				graph.addEdge(int2Node[i], int2Node[j]);
+			}
+		}
+	}
 
     public int[] getPrecoloredTemporaries()
     {
+    	int[] precoloredTemporaries = new int[nasm.getTempCounter()];
+    	for (NasmInst inst : nasm.listeInst) {
+    		colorTemporary(inst.source, precoloredTemporaries);
+    		colorTemporary(inst.destination, precoloredTemporaries);
+		}
+    	return precoloredTemporaries;
     }
+
+    private void colorTemporary(NasmOperand op, int[] precoloredTemporaries) {
+    	if (op == null)
+    		return;
+    	if (op instanceof NasmRegister)
+    		getColor(op, precoloredTemporaries);
+    	if (op instanceof NasmAddress) {
+    		getColor(((NasmAddress) op).base, precoloredTemporaries);
+    		getColor(((NasmAddress) op).offset, precoloredTemporaries);
+		}
+	}
+
+	private void getColor(NasmOperand op, int[] precoloredTemporaries) {
+    	if (op == null || !(op instanceof NasmRegister))
+    		return;
+    	if (op.isGeneralRegister())
+    		precoloredTemporaries[((NasmRegister) op).val] = ((NasmRegister) op).color;
+	}
 
 
     public void allocateRegisters(){
