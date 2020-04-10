3dc4b7ab01ae522c5f9f5011c1a73c0ff5c39575
Simon Crouzet <simon.crouzet@laposte.net>
2020-04-10 19:14:46 +0200

    Ig : v1
    TOUS LES .l FOURNIS SONT DESORMAIS FONCTIONNELS

--- src/ig/Ig.java
+++ src/ig/Ig.java
@@ -8,7 +8,10 @@ import java.util.*;
 import java.io.*;
 
 public class Ig {
+	static final int K = 4;
+
     public Graph graph;
+    public ColorGraph colorGraph;
     public FgSolution fgs;
     public int regNb;
     public Nasm nasm;
@@ -27,35 +30,34 @@ public class Ig {
     public void construction(){
 		initializeNodes();
 		for (NasmInst inst : nasm.listeInst) {
-			manageEdge(fgs.in.get(inst));
-			manageEdge(fgs.out.get(inst));
+			initializeEdges(fgs.in.get(inst));
+			initializeEdges(fgs.out.get(inst));
 		}
     }
 
     private void initializeNodes() {
-		for (NasmInst inst : nasm.listeInst)
+		for (int i=0 ; i<regNb ; i++)
 			graph.newNode();
 		int2Node = graph.nodeArray();
 	}
 
-	// TODO: rename
-	private void manageEdge(IntSet refSet) {
-    	if (refSet.getSize() < 2)
+	private void initializeEdges(IntSet refSet) {
+    	if (refSet.isEmpty())
     		return;
-    	for (int i=0 ; i < nasm.getTempCounter()-1 ; i++) {
+    	for (int i=0 ; i < regNb ; i++) {
     		if (!refSet.isMember(i))
-    			continue;
-    		for (int j= i+1 ; j < nasm.getTempCounter() ; j++) {
+				continue;
+    		for (int j= i+1 ; j < regNb ; j++) {
 				if (!refSet.isMember(j))
 					continue;
-				graph.addEdge(int2Node[i], int2Node[j]);
+				graph.addNOEdge(int2Node[i], int2Node[j]);
 			}
 		}
 	}
 
     public int[] getPrecoloredTemporaries()
     {
-    	int[] precoloredTemporaries = new int[nasm.getTempCounter()];
+    	int[] precoloredTemporaries = new int[graph.nodeCount()];
     	for (NasmInst inst : nasm.listeInst) {
     		colorTemporary(inst.source, precoloredTemporaries);
     		colorTemporary(inst.destination, precoloredTemporaries);
@@ -77,12 +79,36 @@ public class Ig {
 	private void getColor(NasmOperand op, int[] precoloredTemporaries) {
     	if (op == null || !(op instanceof NasmRegister))
     		return;
-    	if (op.isGeneralRegister())
+    	if (op.isGeneralRegister() && ((NasmRegister) op).color != Nasm.REG_UNK)
     		precoloredTemporaries[((NasmRegister) op).val] = ((NasmRegister) op).color;
 	}
 
+	private void color(NasmOperand op, int[] colors) {
+		if (op == null || !(op instanceof NasmRegister))
+			return;
+		if (op.isGeneralRegister() && ((NasmRegister) op).color == Nasm.REG_UNK)
+			((NasmRegister) op).colorRegister(colors[((NasmRegister) op).val]);
+	}
+
+	private void allocateColor(NasmOperand op, int[] colors) {
+		if (op == null)
+			return;
+		if (op instanceof NasmRegister)
+			color(op, colors);
+		if (op instanceof NasmAddress) {
+			color(((NasmAddress) op).base, colors);
+			color(((NasmAddress) op).offset, colors);
+		}
+	}
 
     public void allocateRegisters(){
+    	System.out.println(Arrays.toString(getPrecoloredTemporaries()));
+    	colorGraph = new ColorGraph(graph, K, getPrecoloredTemporaries());
+    	System.out.println(Arrays.toString(colorGraph.couleur));
+    	for (NasmInst inst : nasm.listeInst) {
+    		allocateColor(inst.source, colorGraph.couleur);
+    		allocateColor(inst.destination, colorGraph.couleur);
+		}
     }
 
 
